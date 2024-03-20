1528879f9c3ba7449dd258870eff59409006c27d
Arana Jayavihan <aranajayavihan@gmail.com>
2024-03-21 01:43:00 +0530

    misc

--- config/pkgs/sddm-firewatch-theme-2.nix
+++ config/pkgs/sddm-firewatch-theme-2.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchzip }:
 {
-  sddm-firewatch-theme = stdenv.mkDerivation rec {
-    pname = "firewatch-sddm-theme";
+  sddm-firewatch-theme-2 = stdenv.mkDerivation rec {
+    pname = "firewatch-sddm-theme-2";
     version = "initial";
     dontBuild = true;
     installPhase = ''
       mkdir -p $out/share/sddm/themes
-      cp -aR $src $out/share/sddm/themes/firewatch
+      cp -aR $src $out/share/sddm/themes/firewatch2
     '';
     src = fetchzip {
       url = "https://github.com/Arana-Jayavihan/firewatch-sddm-theme/archive/refs/tags/V2.zip";
