064d08e7f2ec9977e3399da7d0ad94dbf198e6fb
Arana-Jayavihan <aranajayavihan@gmail.com>
2024-06-19 21:26:34 +0530

    Update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Lucifer's NIX";
 
   inputs = {
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
