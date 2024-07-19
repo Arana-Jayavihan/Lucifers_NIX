fad640f7129f8f8d1e97e5a7141b20f62f23a36f
Arana-Jayavihan <aranajayavihan@gmail.com>
2024-07-20 00:01:47 +0530

    Added monitor plug/unplug support to AGS

--- config/home/files/ags-nyx/ags/src/utils/launcher.ts
+++
@@ -1,7 +0,0 @@
-export const getLauncherIcon = (
-    self: { child: { label: string } },
-    windowName: string,
-    visible: any,
-) => {
-    windowName === "launcher" && (self.child.label = visible ? "󱢡" : "󱢦");
-};
--- config/home/files/ags-nyx/ags/src/windows/music/modules/cover.ts
+++
@@ -1,9 +0,0 @@
-import { Widget } from "../../../imports";
-
-export default (player) =>
-	Widget.Box({ className: "cover" }).bind(
-		"css",
-		player,
-		"cover-path",
-		(cover) => `background-image: url('${cover ?? ""}')`,
-	);
