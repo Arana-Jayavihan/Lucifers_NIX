b1654f4288039ef51a6aaa56fca10729ff24e32c
Arana-Jayavihan <aranajayavihan@gmail.com>
2025-11-17 16:35:33 +0530

    misc

--- config/home/packages.nix
+++ config/home/packages.nix
@@ -23,11 +23,6 @@ in {
     tree
     swaylock-effects
 
-<<<<<<< HEAD
-    # Import Scripts
-=======
-    # Import Scripts 
->>>>>>> d91af55 (themechange and btledctl fixes)
     (import ./../scripts/emopicker9000.nix { 
       inherit pkgs;
     })
