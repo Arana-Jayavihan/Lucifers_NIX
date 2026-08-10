f8353ef3e9eb70b09ebdd49eeec657a6a19dd4fb
Arana-Jayavihan <aranajayavihan@gmail.com>
2026-08-10 11:43:34 +0530

    misc

--- config/home/packages.nix
+++ config/home/packages.nix
@@ -116,16 +116,12 @@ in
     (import ./../scripts/claude.nix {
       inherit pkgs;
     })
-<<<<<<< Updated upstream
     (import ./../scripts/launchptenv.nix {
       inherit pkgs;
     })
-=======
     (import ./../scripts/webshare.nix {
       inherit pkgs;
     })
-
->>>>>>> Stashed changes
   ]
   # Host-specific user packages from hosts/<host>/options.nix.
   ++ map resolvePkg (opt.userPackages or [ ]);
